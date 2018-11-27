0453580902152b4f8190793a22af3104cde47d23
Karim ElGhazouly <karimelghazouly@gmail.com>
2018-11-27 15:34:53 +0200

    Added infinte ground and infinite bounces

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class Generate : MonoBehaviour {
 
 	public GameObject bonus;
+	public GameObject player;
+	public GameObject ground;
+	private float lastx;
+	private float last_floor_z = -25;
+	private float last_bonus_z = -25;
 	void Start () {
-		for (int i = 0; i < 100; i++) {
-			float x = Random.Range (-24, 24);
-			float z = Random.Range (-24, 24);
-			Vector3 randvec = new Vector3 (x, 0.5f, z);
-			Quaternion rot = new Quaternion ( Random.Range (-24, 24),  Random.Range (-24, 25),  Random.Range (-25, 25), Random.Range (-25, 25));
-			Instantiate (bonus, randvec, rot);
-		}
+		
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 player_pos = player.transform.position;
+		if (last_floor_z < player_pos.z) {
+			last_floor_z += 25;
+			Vector3 randvec = new Vector3 (0.0f, 0.0f, last_floor_z);
+			Quaternion rot = ground.transform.rotation;
+			Instantiate (ground, randvec ,rot);
+			Debug.Log (randvec);
+			float nextfloorZ = last_floor_z+25;
+			randvec = new Vector3 (0.0f, 0.0f, nextfloorZ);
+			rot = ground.transform.rotation;
+			Instantiate (ground, randvec ,rot);
+			Debug.Log (randvec);
+		}
+		if (last_bonus_z < player_pos.z) {
+			last_bonus_z += 25;
+			Vector3 randvec = new Vector3 (Random.Range(-10,10),1, last_bonus_z);
+			Quaternion rot = new Quaternion (Random.Range (-10, 10), Random.Range (-20, 50), Random.Range (-15, 50), Random.Range (-40, 30));
+			Instantiate (bonus, randvec ,rot);
+		}
 	}
 }
