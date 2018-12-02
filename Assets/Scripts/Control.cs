0616d2ae0d4651ce6828d5b92c8baf45b92b9a01
Karim ElGhazouly <karimelghazouly@gmail.com>
2018-12-02 20:38:05 +0200

    Added counter to count the score

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Control : MonoBehaviour {
-
+	public Text count;
+	public int counter = 0;
 	Rigidbody RD;
 	float speed = 10;
 	void Start () {
 		RD = GetComponent<Rigidbody> ();
+		counter = 0;
+		count.text = "Score: " + counter.ToString();
 	}
 
-	void FixedUpdate () {
+	void Update () {
 		float X = Input.GetAxis ("Horizontal");
 		float Z = Input.GetAxis ("Vertical");
 		Vector3 vec = new Vector3 (X,0.0f,Z);
 		RD.AddForce (vec * speed);
 	}
 	void OnTriggerEnter(Collider other){
-		if(other.gameObject.CompareTag("Pick Up"))
+		if (other.gameObject.CompareTag ("Pick Up")) {
 			DestroyObject (other.gameObject);
+			counter++;
+			count.text = "Score: " + counter.ToString ();
+		}
 	}
 }
