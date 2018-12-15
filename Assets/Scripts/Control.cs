41cacdbe262880ec30c6e19bdfa198b83b2cd71e
Karim ElGhazouly <karimelghazouly@gmail.com>
2018-12-15 19:46:05 +0200

    add script for destroying grounds in fixed interval

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 public class Control : MonoBehaviour {
 	public Text count;
 	public int counter = 0;
+	private float X, Z;
 	Rigidbody RD;
-	float speed = 10;
+	float speed = 20;
+
 	void Start () {
 		RD = GetComponent<Rigidbody> ();
 		counter = 0;
 		count.text = "Score: " + counter.ToString();
 	}
 
-	void Update () {
-		float X = Input.GetAxis ("Horizontal");
-		float Z = Input.GetAxis ("Vertical");
-		Vector3 vec = new Vector3 (X,0.0f,Z);
-		RD.AddForce (vec * speed);
+	void Update(){
+		X = Input.GetAxis ("Horizontal");
+		Z = Input.GetAxis ("Vertical");
+		Vector3 vec = new Vector3 (X*speed, transform.position.y, Z*speed);
+		RD.AddForce (vec);
 	}
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.CompareTag ("Pick Up")) {
--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -4,34 +4,40 @@ using UnityEngine;
 
 
 public class Generate : MonoBehaviour {
-	
+	List<GameObject>Grounds;
 	public GameObject bonus, player, ground, wall;
 	private float lastx;
 	private float last_floor_z = 0, last_bonus_z = -25, last_wall = -10, wall_width, last_check = -25;
+	private int idx = 0;
 	private Vector3 randvec;
 	Quaternion rot;
 	void Start () {
-		
+		Grounds = new List<GameObject> ();
+		InvokeRepeating("DestoryGround", 10.0f,10.0f);
 	}
 		
 	void Update () {
 		Vector3 player_pos = player.transform.position;
-		//if (last_check < player_pos.z) GenGround();
-		//if (last_bonus_z < player_pos.z) GenBonus();
-		//if (last_wall < player_pos.z) GenWall ();
+		if (last_check < player_pos.z) GenGround();
+		if (last_bonus_z < player_pos.z) GenBonus();
+		if (last_wall < player_pos.z) GenWall ();
 	}
+
 	void GenGround()
 	{
 		last_floor_z += 50;
 		last_check = last_check + 25;
 		randvec = new Vector3 (0.0f, 0.0f, last_floor_z);
 		rot = ground.transform.rotation;
-		Instantiate (ground, randvec ,rot);
-		/*last_floor_z += 50;
+		GameObject one = (GameObject)Instantiate (ground, randvec, rot);
+		last_floor_z += 50;
 		randvec = new Vector3 (0.0f, 0.0f, last_floor_z);
 		rot = ground.transform.rotation;
-		Instantiate (ground, randvec ,rot);*/
+		GameObject two = (GameObject)Instantiate (ground, randvec ,rot);
+		Grounds.Add (one);
+		Grounds.Add (two);
 	}
+
 	void GenBonus()
 	{
 		last_bonus_z += 50;
@@ -39,9 +45,17 @@ public class Generate : MonoBehaviour {
 		rot = new Quaternion (Random.Range (-10, 10), Random.Range (-20, 50), Random.Range (-15, 50), Random.Range (-40, 30));
 		Instantiate (bonus, randvec ,rot);
 	}
+
+	void DestoryGround()
+	{
+		Debug.Log ("aa");
+		DestroyObject (Grounds [idx]);
+		idx++;
+	}
+
 	void GenWall()
 	{
-		last_wall += 25;
+		last_wall += 50;
 		wall_width = Random.Range (3, 10);
 		randvec = new Vector3 (Random.Range (-25 + 1 + wall_width, 25 - wall_width - 1), 0.5f, last_wall);
 		rot = wall.transform.rotation;
